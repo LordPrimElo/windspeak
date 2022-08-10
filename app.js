747be543c642c09be00f3cc01a66c9cf02105f95
LordPrimElo <evansrathore@gmail.com>
2022-08-10 15:13:23 +0530

    Add ejs and boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+const mainRoutes = require("./routes/main")
+
+app.use(mainRoutes)
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("It's Alive!")
 })
